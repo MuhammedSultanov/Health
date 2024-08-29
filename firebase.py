8fa83eddc080b748d4885ed66f0231eaaea8da5a
MuhammedSultanov <9073@holbertonstudents.com>
2024-08-29 13:02:09 +0400

    db

--- firebase.py
+++ firebase.py
@@ -1,9 +1,10 @@
 import firebase_admin
-from firebase_admin import db
+from firebase_admin import db, credentials
+import json
 
-cred_obj = firebase_admin.credentials.Certificate('key.json')
+# Initialize Firebase app with service account
+cred_obj = credentials.Certificate('key.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
-	'databaseURL': "https://console.firebase.google.com/project/healthcare-4bee0/database/healthcare-4bee0-default-rtdb/data/~2F"
-	})
+    'databaseURL': "https://newhealth-f6c05-default-rtdb.firebaseio.com/"  # Replace with your actual database URL
+})
 
-ref = db.reference("/")
