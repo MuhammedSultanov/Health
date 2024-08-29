dbd4186e36c215b39ff9785260f028643d04412f
CashlockH <ilyashesenli48@gmail.com>
2024-08-29 12:09:58 +0400

    connecting to database

--- firebase.py
+++ firebase.py
@@ -1,6 +1,9 @@
 import firebase_admin
+from firebase_admin import db
 
 cred_obj = firebase_admin.credentials.Certificate('key.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
 	'databaseURL': "https://console.firebase.google.com/project/healthcare-4bee0/database/healthcare-4bee0-default-rtdb/data/~2F"
 	})
+
+ref = db.reference("/")
