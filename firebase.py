54d0069e4ebe921bf425ec954914d722ec990e5f
CashlockH <ilyashesenli48@gmail.com>
2024-08-29 11:34:52 +0400

    database key

--- firebase.py
+++ firebase.py
@@ -1,2 +1,6 @@
 import firebase_admin
 
+cred_obj = firebase_admin.credentials.Certificate('key.json')
+default_app = firebase_admin.initialize_app(cred_object, {
+	'databaseURL': "https://console.firebase.google.com/project/healthcare-4bee0/database/healthcare-4bee0-default-rtdb/data/~2F"
+	})
